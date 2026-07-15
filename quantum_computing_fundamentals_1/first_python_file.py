fa1c7d31362e59ae3e860c4398f6d4834b0e8fcd
Srikari2386 <srikarikona2312@gmail.com>
2026-07-15 14:16:32 -0400

    Initial commit

---
+++ quantum_computing_fundamentals_1/first_python_file.py
@@ -0,0 +1,17 @@
+# Create 3 variables. An int, a string, and a boolean
+
+name = "Srikari"
+number = 12
+at_DAE = True
+
+# If statement. If your int variable is greater than 3. Display the message "Yes it's greater than 3" else display, "It's not greater than 3"
+
+if number > 3:
+    print("Yes, your number is greater than 3")
+else:
+    print("No, your number is not greater then 3")
+
+# Create code that asks a user for their name and then display what the user typed
+
+name = input("What's your name: ")
+print(name)
