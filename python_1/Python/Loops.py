beb1a5fa49a0a9e43f1f812a876ccd3f518352b9
Srikari2386 <srikarikona2312@gmail.com>
2026-08-05 14:18:18 -0400

    I leaned how to put in codes and markdowns on VS code. I also workedon IBM Quantum Platform.

---
+++ python_1/Python/Loops.py
@@ -0,0 +1,5 @@
+for number in range(15):
+    print("Srikari")
+
+for number in range(1,11):
+    print("Room",number, "checked")
---
+++ python_1/Python/library.py
@@ -0,0 +1,4 @@
+import math
+print(math.sqrt(16))
+print(math.pi)
+print(math.floor(4.7))
